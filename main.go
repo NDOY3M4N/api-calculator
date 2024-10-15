195e395cd232ffededff95945fe8be84c7084c40
Abdoulaye NDOYE <pa.ndoye@outlook.fr>
2024-10-15 16:06:24 +0000

    feat: add handler for addition

--- main.go
+++ main.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"io"
+	"log/slog"
+	"net/http"
+	"os"
+)
+
+type Payload struct {
+	Number1 int `json:"number1"`
+	Number2 int `json:"number2"`
+}
+
+func addHandler(w http.ResponseWriter, r *http.Request) {
+	var req Payload
+	if err := parseJSON(r, &req); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	writeJSON(w, http.StatusOK, map[string]int{"result": req.Number1 + req.Number2})
+}
 
 func main() {
-	fmt.Println("hi mom")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("POST /add", addHandler)
+
+	server := http.Server{
+		Handler: mux,
+		Addr:    ":3000",
+	}
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	logger.Info("Server started on port 3000")
+
+	if err := server.ListenAndServe(); err != nil {
+		logger.Error("Error", err)
+	}
+}
+
+func parseJSON(r *http.Request, v any) error {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
+
+	return json.Unmarshal(body, v)
+}
+
+func writeJSON(w http.ResponseWriter, status int, v any) error {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	return json.NewEncoder(w).Encode(v)
 }
