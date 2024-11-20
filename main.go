f7b8a329a26aabbd803cf737073d2adbff846ccb
Abdoulaye NDOYE <pa.ndoye@outlook.fr>
2024-11-20 16:26:17 +0000

    feat: integrate Swagger

--- main.go
+++ main.go
@@ -6,6 +6,11 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
+	"strings"
+
+	httpSwagger "github.com/swaggo/http-swagger/v2"
+
+	_ "github.com/NDOY3M4N/api-calculator/docs"
 )
 
 type Payload struct {
@@ -13,6 +18,25 @@ type Payload struct {
 	Number2 int `json:"number2"`
 }
 
+type APIError struct {
+	Error string
+}
+
+type APISuccess struct {
+	Result int
+}
+
+// addHandler Foo
+//
+// @summary Add two numbers
+// @description Add two numbers together
+// @tags Math
+// @accept json
+// @produce json
+// @param payload body Payload true "Numbers needed for the operation"
+// @success 200 {object} APISuccess
+// @failure 400 {object} APIError
+// @router /add [post]
 func addHandler(w http.ResponseWriter, r *http.Request) {
 	payload, err := parseJSON(r)
 	if err != nil {
@@ -23,6 +47,17 @@ func addHandler(w http.ResponseWriter, r *http.Request) {
 	writeJSON(w, http.StatusOK, map[string]int{"result": payload.Number1 + payload.Number2})
 }
 
+// substractHandler Foo
+//
+// @summary Substract two numbers
+// @description Substract two numbers together
+// @tags Math
+// @accept json
+// @produce json
+// @param payload body Payload true "Numbers needed for the operation"
+// @success 200 {object} APISuccess
+// @failure 400 {object} APIError
+// @router /substract [post]
 func substractHandler(w http.ResponseWriter, r *http.Request) {
 	payload, err := parseJSON(r)
 	if err != nil {
@@ -33,6 +68,17 @@ func substractHandler(w http.ResponseWriter, r *http.Request) {
 	writeJSON(w, http.StatusOK, map[string]int{"result": payload.Number1 - payload.Number2})
 }
 
+// multiplyHandler Foo
+//
+// @summary Multiply two numbers
+// @description Multiply two numbers together
+// @tags Math
+// @accept json
+// @produce json
+// @param payload body Payload true "Numbers needed for the operation"
+// @success 200 {object} APISuccess
+// @failure 400 {object} APIError
+// @router /multiply [post]
 func multiplyHandler(w http.ResponseWriter, r *http.Request) {
 	payload, err := parseJSON(r)
 	if err != nil {
@@ -43,6 +89,17 @@ func multiplyHandler(w http.ResponseWriter, r *http.Request) {
 	writeJSON(w, http.StatusOK, map[string]int{"result": payload.Number1 * payload.Number2})
 }
 
+// divideHandler Foo
+//
+// @summary Divide two numbers
+// @description Divide two numbers together
+// @tags Math
+// @accept json
+// @produce json
+// @param payload body Payload true "Numbers needed for the operation"
+// @success 200 {object} APISuccess
+// @failure 400 {object} APIError
+// @router /divide [post]
 func divideHandler(w http.ResponseWriter, r *http.Request) {
 	payload, err := parseJSON(r)
 	if err != nil {
@@ -58,16 +115,49 @@ func divideHandler(w http.ResponseWriter, r *http.Request) {
 	writeJSON(w, http.StatusOK, map[string]int{"result": payload.Number1 / payload.Number2})
 }
 
-func main() {
-	mux := http.NewServeMux()
+// @title         Calculator API
+// @version       1.0
+// @description   This is a sample server for Calculator API
 
-	mux.HandleFunc("POST /add", addHandler)
-	mux.HandleFunc("POST /substract", substractHandler)
-	mux.HandleFunc("POST /multiply", multiplyHandler)
-	mux.HandleFunc("POST /divide", divideHandler)
+// @contact.name  Abdoulaye NDOYE
+// @contact.url   https://github.com/NDOY3M4N
+// @contact.email pa.ndoye@outlook.com
+
+// @license.name  MIT
+// @license.url   https://github.com/NDOY3M4N/calculator-api/blob/main/LICENSE
+
+// @host          localhost:3000
+// @BasePath      /api/v1
+func main() {
+	router := http.NewServeMux()
+
+	router.HandleFunc("POST /add", addHandler)
+	router.HandleFunc("POST /substract", substractHandler)
+	router.HandleFunc("POST /multiply", multiplyHandler)
+	router.HandleFunc("POST /divide", divideHandler)
+
+	// Create a separate handler for Swagger
+	swaggerHandler := http.StripPrefix("/swagger/", http.HandlerFunc(
+		httpSwagger.Handler(
+			httpSwagger.URL("http://localhost:3000/swagger/doc.json"),
+			httpSwagger.DefaultModelsExpandDepth(httpSwagger.HideModel),
+		),
+	))
+
+	v1 := http.NewServeMux()
+	v1.Handle("/api/v1/", http.StripPrefix("/api/v1", router))
+
+	// Combine the v1 handler and the Swagger handler
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/swagger/" || strings.HasPrefix(r.URL.Path, "/swagger/") {
+			swaggerHandler.ServeHTTP(w, r)
+		} else {
+			v1.ServeHTTP(w, r)
+		}
+	})
 
 	server := http.Server{
-		Handler: mux,
+		Handler: handler,
 		Addr:    ":3000",
 	}
 
